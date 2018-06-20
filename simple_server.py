b264fe6515c7c848ecb5443d9f4401faf6421952
momo setti <zinasetti@gmail.com>
2018-06-20 10:24:45 +0100

    :zap: add handel function expection for shutting down the server

--- simple_server.py
+++ simple_server.py
@@ -4,7 +4,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
  
 # HTTPRequestHandler class
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
-  # GET
+  # GET http verb
   def do_GET(self):
         # Send response status code
         self.send_response(200)
@@ -13,24 +13,26 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
  
         # Send message back to client
-        message = "Hello world!"
+        message = "<h1>Hello world</h1>"
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
- 
-def run(port):
-      
-  print('Starting server ...')
-  # Server settings
-  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-  server_address = ('127.0.0.1', port)
-  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
-  host='http://localhost:%s'%port
-  print('\033[0;37;46m Running server at',host)
-  httpd.serve_forever()
+
 if __name__ == "__main__":
     from sys import argv
 
     if len(argv) == 2:
-        run(port=int(argv[1]))
-run(port)
+        port=int(argv[1])
+
+try:
+      print('Starting server ...')
+      # Server settings
+      # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+      server_address = ('127.0.0.1', port)
+      httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+      host='http://localhost:%s'%port
+      print(' \033[0;37;46m Running server at',host)
+      httpd.serve_forever()
+except KeyboardInterrupt:
+      print("^C received, shutting down the web server")
+      httpd.socket.close()
